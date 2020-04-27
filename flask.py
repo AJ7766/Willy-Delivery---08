11961bafd18139ea05490d9d65bf4be8f3237fd3
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2020-04-27 23:40:20 +0200

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+@app.route('/')
+@app.route('/hem')
+def home():
+    return render_template('hem.html', title='Hem')
+
+@app.route('/sorter')
+def sorter():
+    return render_template('sorter.html', title='Sorter')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html', title='kontakt')
+
+@app.route('/omoss')
+def omoss():
+    return render_template('omoss.html', title='Om oss')
+
+@app.route('/erbjudande')
+def erbjudande():
+    return render_template('erbjudande.html', title='Erbjudanen')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
