8afeb78e38f2f5e2741a3e18acd4a868457e7ac3
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2020-05-07 15:14:45 +0200

    Update flask.py

--- flask.py
+++ flask.py
@@ -42,7 +42,17 @@ def admin():
 
 @app.route('/sok', methods=['GET', 'POST'])
 def sok():
-    sokt = request.form['searchBar']
+    sokt = request.form['searchBar'] #hämtar från input 
+    fel = False
+    if "sur" in sokt:
+        sokt = 'sur'
+    if "söt" in sokt:
+        sokt = 'söt'
+    if "dödsk" in sokt or 'skalle' in sokt:
+        sokt = 'dödskalle'
+    if "bubb" in sokt or 'liz' in sokt:
+        sokt = 'bubblizz'
+        #SELECT * FROM candy WHERE name LIKE "%god%"
     cursor.execute("select count(*) from produkt where namn='{0}'".format(sokt));
     antalp = cursor.fetchall()
     cursor.execute("select count(*) from sort where namn='{0}'".format(sokt));
@@ -51,12 +61,12 @@ def sok():
         cursor.execute("select * from produkt where namn='{0}'".format(sokt)); 
         sok = cursor.fetchall()
     elif antals[0][0] > 0:
-        cursor.execute("SELECT produkt.* FROM produkt, sort WHERE sort.namn='{0}'".format(sokt));
+        cursor.execute("select produkt.* from produkt join sort on produkt.namn=sort.p_namn where sort.namn='{0}'".format(sokt));
         sok = cursor.fetchall() 
-        #gör en join så vi får ut korrekt 
     else: 
         sok='tyvärr fanns inte det du letar efter' 
-    return render_template('sok.html', title='Sök', produkt = sok)
+        fel = True 
+    return render_template('sok.html', title='Sök', produkt = sok, fel = fel)
 
 
 if __name__ == '__main__':
