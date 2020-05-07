2262b3d63b0ff5dc2acae77e2e6a773ee0ac7833
AJ7766 <39444662+AJ7766@users.noreply.github.com>
2020-05-07 21:53:36 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,56 @@
+//sök fukntion 
+function active(){
+    var searchBar = document.getElementById('searchBar');
+
+    if(searchBar.value == 'Sök på produkt...'){
+        searchBar.value = ''
+        searchBar.placeholder = 'Sök på produkt...'
+    }}
+function inactive(){
+    var searchBar = document.getElementById('searchBar');
+
+    if(searchBar.value == ''){
+        searchBar.value = 'Sök på produkt...'
+        searchBar.placeholder = ''
+    }}
+
+//sök fukntion end
+
+// meny mobil
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links a');
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = '';
+        }
+        else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+      });
+      burger.classList.toggle('toggle');
+    }); }  
+
+// meny mobil end
+
+// slide show
+    navSlide();
+const slideshowImages = document.querySelectorAll(".slideshow-img");
+
+const nextImagesDelay = 3000;
+
+let currentImagesCounter = 0;
+
+slideshowImages[currentImagesCounter].style.display = "block";
+
+setInterval(nextImages, nextImagesDelay);
+
+function nextImages(){
+    slideshowImages[currentImagesCounter].style.display = "none";
+    currentImagesCounter = (currentImagesCounter + 1) % slideshowImages.length;
+    slideshowImages[currentImagesCounter].style.display = "block";
+}
+// slide show
