2effadaf9b74af5c5ce0c471b8e87ac9e723eda9
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2020-05-15 22:08:31 +0200

    varukorg

--- static/varukorg.js
+++ static/varukorg.js
@@ -1,16 +1,3 @@
-var product = [
-    {name: 'bubblizz', 
-    inCart: 0},
-    {name: 'Kexchoklad mini', 
-    inCart: 0},
-    {name: 'Rosa jordgubbsremmar', 
-    inCart: 0},
-    {name: 'Sockerbitar', 
-    inCart: 0},
-    {name: 'Riesen', 
-    inCart: 0}
-];
-
 let carts = document.querySelectorAll('.shop-button');
 
 function add(e){
@@ -19,6 +6,7 @@ function add(e){
     alert(e.id);
     var pro = e.id;
     localStorage.setItem('produkter', pro);
+    cartNumbers(pro);
 }
 
 function onLoadCartNumbers(){
@@ -29,14 +17,9 @@ function onLoadCartNumbers(){
     }
 }
 
-for (let i = 0; i < carts.length; i++){
-    carts[i].addEventListener('click', () =>{
-        cartNumbers();
-    })
-}
-
-function cartNumbers(product){
-    let productNumbers = localStorage.getItem('cartNumbers');
+function cartNumbers(pro){
+    console.log('the product clicked is', pro);
+    productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
     if(productNumbers){
         localStorage.setItem('cartNumbers', productNumbers + 1);
