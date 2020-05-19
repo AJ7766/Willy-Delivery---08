6894b046032ec61eecd39301b2c23956af13dc1a
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2020-05-19 13:26:37 +0200

    fixat klar fuktionen
    fixat klar fuktionen så varornas namn som är trycka  på sparas

--- static/varukorg.js
+++ static/varukorg.js
@@ -3,27 +3,28 @@ let carts = document.querySelectorAll('.shop-button');
 let pro = [];
 
 
+
 function add(e){ 
     e = e || window.event;
     e = e.target || e.scrElement;
+    pro.push(localStorage.getItem('produkter'));
     pro.push(e.id);
     localStorage.setItem('produkter', pro); //läger in i localstorage vilket produkt som tryckts på
     cartNumbers(pro);
+    pro = []
 } // Fuktionen hämtar id från knappen som användaren tryck på och skickar vidare den till en annan funktion
 
 function onLoadCartNumbers(){
     let productNumbers = localStorage.getItem('cartNumbers');
+    let products = localStorage.getItem('produkter');
 
     if(productNumbers){
         document.querySelector('.cart span').textContent = productNumbers;
     }
 } // laddar hur många produter som finns direkt när hemsidan öppnas och laddas om
-1
+
 function cartNumbers(pro){
     console.log('the product clicked is', pro);
-
-    pro.push(localStorage.getItem('product'));
-    localStorage.setItem('produkter', pro);
 
     productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
