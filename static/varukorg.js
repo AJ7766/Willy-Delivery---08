78066b31053069c23f171107741967e923d7e776
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2020-05-19 11:29:45 +0200

    Update varukorg.js

--- static/varukorg.js
+++ static/varukorg.js
@@ -1,13 +1,14 @@
 let carts = document.querySelectorAll('.shop-button');
+//sparar alla buttons i en variabel
 
-function add(e){
+function add(e){ 
     e = e || window.event;
     e = e.target || e.scrElement;
     alert(e.id);
     var pro = e.id;
-    localStorage.setItem('produkter', pro);
+    localStorage.setItem('produkter', pro); //läger in i localstorage vilket produkt som tryckts på
     cartNumbers(pro);
-}
+} // Fuktionen hämtar id från knappen som användaren tryck på och skickar vidare den till en annan funktion
 
 function onLoadCartNumbers(){
     let productNumbers = localStorage.getItem('cartNumbers');
@@ -15,7 +16,7 @@ function onLoadCartNumbers(){
     if(productNumbers){
         document.querySelector('.cart span').textContent = productNumbers;
     }
-}
+} // laddar hur många produter som finns direkt när hemsidan öppnas och laddas om
 
 function cartNumbers(pro){
     console.log('the product clicked is', pro);
@@ -29,7 +30,6 @@ function cartNumbers(pro){
         localStorage.setItem('cartNumbers',  1);
         document.querySelector('.cart span').textContent = 1;
     }
-
-}
+}// funktionen håller koll på hur många varor som lagts till i varukorgen
 
 onLoadCartNumbers();
